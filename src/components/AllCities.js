7e03d5e80e84e4ec0ccc7738fc7570548dafebf9
r-shahid <rianashahid@gmail.com>
2026-08-28 17:19:20 -0400

    add coming soon

--- src/components/AllCities.js
+++ src/components/AllCities.js
@@ -6,7 +6,7 @@ const AllCities = ({cities}) => {
 
     // console.log(cities.toReversed().at(-1))
     //change top link to go back to home if city.city matches cities.at(-1).city
-    
+
   return (
     <div className="AllCities">
       <header>
@@ -16,17 +16,18 @@ const AllCities = ({cities}) => {
       <div className="cities_list">
         {cities.toReversed().map((city)=>{
             return(
-                <Link to={`/${city.city}`}>
-                <div className="trip">
-                    <h2 className="city-state">{city.city}<span className="state"> {city.state}</span></h2>
-                    <div className="duration">
-                        {city.trip_start} to {city.trip_end}
-                    </div>
-                    <div className="thumbnail">
-                        <img src={`/images/${city.thumbnail}`} alt={city.thumbnail_alt}/>
-                    </div>
-                </div>
-                </Link>
+                // <Link to={`/${city.city}`}>
+                // <div className="trip">
+                //     <h2 className="city-state">{city.city}<span className="state"> {city.state}</span></h2>
+                //     <div className="duration">
+                //         {city.trip_start} to {city.trip_end}
+                //     </div>
+                //     <div className="thumbnail">
+                //         <img src={`/images/${city.thumbnail}`} alt={city.thumbnail_alt}/>
+                //     </div>
+                // </div>
+                // </Link>
+                <div className="coming-soon">More Cities coming soon</div>
             )
         })}
       </div>
