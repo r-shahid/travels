194b03fa8ee20b6e4caee51ac3f9f7dfb4fdbbf1
r-shahid <rianashahid@gmail.com>
2026-09-16 20:32:28 -0400

    add some pics

--- src/components/AllCities.js
+++ src/components/AllCities.js
@@ -1,8 +1,7 @@
 import { Link } from "react-router-dom";
 import "../styles/allcities.css";
 
-const AllCities = ({cities}) => {
-
+const AllCities = ({ cities }) => {
   return (
     <div className="AllCities">
       <header>
@@ -10,22 +9,28 @@ const AllCities = ({cities}) => {
       </header>
       <h1>All Cities</h1>
       <div className="cities_list">
-        {cities.toReversed().map((city, key)=>{
-            return(
-                <Link to={key === 0 ? "/" : `/${city.city}`} key={key}>
-                <div className="trip">
-                    <h2 className="city-state">
-                        <div className="city">{city.city}</div>
-                        <div className="state"> {city.state}</div></h2>
-                    <div className="duration">
-                        {city.trip_start} to {city.trip_end}
-                    </div>
-                    <div className="thumbnail">
-                        <img src={`/images/${city.thumbnail}`} alt={city.thumbnail_alt}/>
-                    </div>
+        {cities.toReversed().map((city, key) => {
+          return (
+            <Link to={key === 0 ? "/" : `/${city.city}`} key={key}>
+              <div className="trip">
+                <div className="info">
+                  <h2 className="city-state">
+                    <div className="city">{city.city}</div>
+                    <div className="state"> {city.state}</div>
+                  </h2>
+                  <div className="duration">
+                    {city.trip_start} to {city.trip_end}
+                  </div>
                 </div>
-                </Link>
-            )
+                <div className="thumbnail">
+                  <img
+                    src={`/images/${city.thumbnail}`}
+                    alt={city.thumbnail_alt}
+                  />
+                </div>
+              </div>
+            </Link>
+          );
         })}
       </div>
     </div>
